c185f8c06ea26ffdaedbf2517b18e8efd6801b8b
Hausec <33264718+hausec@users.noreply.github.com>
2020-10-12 16:07:43 -0400

    Fixed AAD connect check

--- AzureHound.ps1
+++ AzureHound.ps1
@@ -14,9 +14,12 @@ function Get-PrincipalMap {
     $PrincipalMap
 }
 function Connect-AADUser {
+    $ConnectionTest = try{ [Microsoft.Open.Azure.AD.CommonLibrary.AzureSession]::AccessTokens['AccessToken']}
+    catch{"Error"}
+    If($ConnectionTest -eq 'Error'){ 
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
     $aadToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.windows.net").AccessToken
-    Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id
+    Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id}
 }
 
 function Get-AzureGraphToken
@@ -84,16 +87,7 @@ function Invoke-AzureHound {
             exit
             }
     }
-    $AADUser = Get-AzureADTenantDetail *>&1 
-    if ($AADUser -eq $null){
-        Connect-AADUser
-        $AADUser = Get-AzureADTenantDetail *>&1 
-        if ($AADUser -eq $null){
-            Read-Host -Prompt "Login via AzureAD PS Module failed."
-            exit
-            }
-    }
-
+    Connect-AADUser
     $Headers = Get-AzureGraphToken
 
     If(!$TenantID){
