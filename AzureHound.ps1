eea29b707439e7bdd6011af6771c898e0edf4f04
David Nicoll <david.nicoll@open-money.co.uk>
2020-12-03 20:58:52 -0400

    Bug fix: catch when the graph API token has expired, to get a new one
    FIx spacing

--- AzureHound.ps1
+++ AzureHound.ps1
@@ -16,7 +16,7 @@ function Get-PrincipalMap {
 function Connect-AADUser {
     $ConnectionTest = try{ [Microsoft.Open.Azure.AD.CommonLibrary.AzureSession]::AccessTokens['AccessToken']}
     catch{"Error"}
-    If($ConnectionTest -eq 'Error'){ 
+    If($ConnectionTest -eq 'Error' -or $null -eq $ConnectionTest){ 
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
     $aadToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.windows.net").AccessToken
     Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id}
